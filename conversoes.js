f2b9ef186fca07fc178ae8c1f99e0c976fb4655d
Danilo Araujo <dnlysid@gmail.com>
2021-11-25 14:09:48 -0300

    codigos var e conversoes

--- conversoes.js
+++ conversoes.js
@@ -1,7 +1,6 @@
 // tipo de dado
 // booleanos
 
-// conversão implícita
 const numero = 7;
 const numeroString = "7";
 
@@ -11,9 +10,6 @@ console.log(numero == numeroString); // true
 
 // quando soma uma string com um numero dessa forma ele apenas vai concatenar os dois valores
 console.log(numero + numeroString); // 77
-
-
-//conversão explícita
 
 // para somar uma string com numero é da forma abaixo usando Number(), ou String()
 console.log(numero + Number(numeroString)); // 14
