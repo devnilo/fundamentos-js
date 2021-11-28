2b6201cf9f9379c2836029633a461d0b5a0b6eca
Danilo Araujo <dnlysid@gmail.com>
2021-11-28 12:20:22 -0300

    aprendendo mais sobre os arrays

--- arrays/juntando-arrays.js
+++ arrays/juntando-arrays.js
@@ -7,3 +7,7 @@ const salaJavaScript = ['Henrique', 'Tayane', 'Breno'];
 const novaSala = salaPython.concat(salaJavaScript);
 console.log(novaSala); // [ 'Danilo', 'Victória', 'Junior', 'Henrique', 'Tayane', 'Breno' ]
 
+/* concat() é um método útil quando não se deseja alterar o array original, e sim fazer uma cópia alterada. 
+Caso isso não seja necessário, considere utilizar push() ou splice() para inserir novos elementos ou fazer alterações 
+no array original.
+*/
