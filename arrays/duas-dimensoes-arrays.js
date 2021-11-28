e916ac2bcee1f0adbd55a265c37601f601f3a84f
Danilo Araujo <dnlysid@gmail.com>
2021-11-28 12:08:26 -0300

    aprendendo mais sobre os arrays

--- arrays/duas-dimensoes-arrays.js
+++ arrays/duas-dimensoes-arrays.js
@@ -30,3 +30,19 @@ console.log(`${listaAlunosEMedias[0][0]}, sua média é ${listaAlunosEMedias[1][
 console.log(`${listaAlunosEMedias[0][1]}, sua média é ${listaAlunosEMedias[1][1]}`) // Maria, sua média é 7
 console.log(`${listaAlunosEMedias[0][2]}, sua média é ${listaAlunosEMedias[1][2]}`) // Juliana, sua média é 9
 console.log(`${listaAlunosEMedias[0][3]}, sua média é ${listaAlunosEMedias[1][3]}`) // Caio, sua média é 6
+
+//------------------------------------------------------------------------------------------------------------------//
+
+/* Outra forma de chamar os arrays de 2 dimensões é matrizes. Matrizes podem ser visualizadas como uma tabela do Excel, 
+onde cada campo representa um elemento, e precisamos de uma linha e uma coluna para acessar um campo. 
+Então, as linhas são o primeiro colchete e a coluna o segundo colchete.
+
+Arrays têm seu principal uso quando temos uma grande quantidade de informações com propósitos similares, 
+como as notas de um aluno. Matrizes, por sua vez, são utilizadas quando precisamos de vários arrays similares ou 
+com informações ligadas às outras, como as notas de uma classe com vários alunos.
+
+As matrizes não são limitadas a 2 dimensões, podendo ter mais dimensões, de modo que cada dimensão é representada por 
+um colchete. Porém, tome cuidado para não se perder dentro delas, já que se uma matriz passa a ter, 
+por exemplo, 4 ou 5 dimensões, se torna bem difícil saber exatamente o que está sendo acessado e começamos 
+a ter problemas para fazer a manutenção do código.
+*/
